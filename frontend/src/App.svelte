f7b0097f9b70630767f05eee06890eb2b6d0058b
tiero <3596602+tiero@users.noreply.github.com>
2021-12-07 11:43:54 +0100

    Add Gh Action

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,5 @@
-<script type="ts">
-    import Connect from 'svelte-marina-button';
+<script>
+  import Connect from 'svelte-marina-button';
 </script>
 
 <section class="hero has-background-black-bis is-fullheight">
@@ -13,9 +13,7 @@
   <div class="hero-body">
     <div class="container is-max-desktop">
       <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-
-        </div>
+        <div class="column is-half is-offset-one-quarter" />
       </div>
     </div>
   </div>
@@ -24,7 +22,7 @@
     <div class="container is-max-desktop">
       <div class="columns is-mobile is-half is-offset-one-third">
         <div class="column has-text-centered">
-          Made with ❤️ at Vulpem Ventures 
+          Made with ❤️ at Vulpem Ventures
         </div>
       </div>
     </div>
